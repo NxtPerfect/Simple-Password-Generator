29bede979cf4db45c3343b7cfc4d2f6342ab7dc8
NxtPerfect <alakaxan@protonmail.com>
2022-08-06 16:59:19 +0200

    Added checks if we didn't already have special numbers and so on in char list, to not duplicate them, fixed some syntax errors

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import random
 
+# two underscores in class make method/variable private
+
 CHARS = []
 LOWERCASE_LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k',
                      'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'w', 'v', 'x', 'y', 'z']
@@ -8,7 +10,7 @@ VERSION = '0.1.0'
 
 def get_password(self, chars, length: int, seed: int):
     for x in range(length):
-        index = random.randrange(0, (len(chars))
+        index = random.randrange(0, len(chars))
         password = LOWERCASE_LETTERS[index]
         # load password encrypted into json with the seed using caesar's algorithm
     return password
@@ -19,10 +21,10 @@ def generate_seed(self, length: int):
     pass
 
 
-def add_chars_to_list(self, chars):
-    if chars not in CHARS:
-        CHARS += chars
-    pass
+# def add_chars_to_list(self, chars):
+#     if chars not in CHARS:
+#         CHARS += chars
+#     pass
 
 
 if __name__ == "__main__":
@@ -31,20 +33,24 @@ if __name__ == "__main__":
     use_special_chars = False
     use_capital_letters = False
     use_numbers = False
-    menu = "Welcome to Password Generator v%s\nPick your option:\n1. Generate password\n2. Change password length %s\n3. Include special characters %b\n4. Include capital letters %b\n5. Include numbers %b\t" % (VERSION, length, use_special_chars, use_capital_letters, use_numbers))
+    menu = "Welcome to Password Generator v%s\nPick your option:\n1. Generate password\n2. Change password length %s\n3. Include special characters %s\n4. Include capital letters %s\n5. Include numbers %s\t" % (VERSION, length, str(use_special_chars), str(use_capital_letters), str(use_numbers))
     choice = int(input())
     match choice:
         case 1:
-            print(get_password(CHARS, length, generate_seed(random.randint(0,9))
+            password = get_password(CHARS, length, generate_seed(6))
+            print(password)
             input()
         case 2:
             while True:
-                length = int(input("How long should the password be?\t")
+                length = int(input("How long should the password be?\t"))
                 if length != 0:
                     break
         case 3:
-            CHARS += SPECIAL_CHARACTERS
+            if SPECIAL_CHARACTERS not in CHARS:
+                CHARS += SPECIAL_CHARACTERS
         case 4:
-            CHARS += CAPITAL_LETTERS
+            if CAPITAL_LETTERS not in CHARS:
+                CHARS += CAPITAL_LETTERS
         case 5:
-            CHARS += NUMBERS
+            if NUMBERS not in CHARS:
+                CHARS += NUMBERS
