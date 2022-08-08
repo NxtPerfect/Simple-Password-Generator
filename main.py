977a0d9d5d14d68c405bf9ce325bf6d5dda6a4d2
NxtPerfect <alakaxan@protonmail.com>
2022-08-08 12:42:47 +0200

    Added special characters, capital letters and numbers list, fixed menu not changing value of bools for corresponding options, fixed functions that took argument "self" while not being in class. Fixed the way we generated password, generate_seed function is obsolete as of now but will be used later on when fixing randomness

--- main.py
+++ main.py
@@ -2,22 +2,29 @@ import random
 
 # two underscores in class make method/variable private
 
-CHARS = []
+CHARS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k',
+         'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'w', 'v', 'x', 'y', 'z']
 LOWERCASE_LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k',
                      'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'w', 'v', 'x', 'y', 'z']
+CAPITAL_LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K',
+                   'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'W', 'V', 'X', 'Y', 'Z']
+SPECIAL_CHARACTERS = ['~','!','@','#','$','%','^','&','*','(',')','-','_','=','+','[',']','{','}',':',';','/','<','>','.','?','`']
+NUMBERS = ['1','2','3','4','5','6','7','8','9','0']
 VERSION = '0.1.0'
 
 
-def get_password(self, chars, length: int, seed: int):
+def get_password(chars: list, length: int):
+    password = ''
     for x in range(length):
-        index = random.randrange(0, len(chars))
-        password = LOWERCASE_LETTERS[index]
+        index = random.randint(0, (len(chars)-1))
+        password += chars[index]
         # load password encrypted into json with the seed using caesar's algorithm
     return password
 
-def generate_seed(self, length: int):
+
+def generate_seed(length: int):
     for x in range(length):
-        seed = rand.random(0, 9)
+        seed = random.randint(0, 9)
     pass
 
 
@@ -33,24 +40,29 @@ if __name__ == "__main__":
     use_special_chars = False
     use_capital_letters = False
     use_numbers = False
-    menu = "Welcome to Password Generator v%s\nPick your option:\n1. Generate password\n2. Change password length %s\n3. Include special characters %s\n4. Include capital letters %s\n5. Include numbers %s\t" % (VERSION, length, str(use_special_chars), str(use_capital_letters), str(use_numbers))
-    choice = int(input())
-    match choice:
-        case 1:
-            password = get_password(CHARS, length, generate_seed(6))
-            print(password)
-            input()
-        case 2:
-            while True:
-                length = int(input("How long should the password be?\t"))
-                if length != 0:
-                    break
-        case 3:
-            if SPECIAL_CHARACTERS not in CHARS:
-                CHARS += SPECIAL_CHARACTERS
-        case 4:
-            if CAPITAL_LETTERS not in CHARS:
-                CHARS += CAPITAL_LETTERS
-        case 5:
-            if NUMBERS not in CHARS:
-                CHARS += NUMBERS
+    while True:
+        menu = "Welcome to Password Generator v%s\nPick your option:\n1. Generate password\n2. Change password length %s\n3. Include special characters %s\n4. Include capital letters %s\n5. Include numbers %s\t" % (
+        VERSION, length, str(use_special_chars), str(use_capital_letters), str(use_numbers))
+        choice = int(input(menu))
+        match choice:
+            case 1:
+                password = get_password(CHARS, length)
+                print(password)
+                input()
+            case 2:
+                while True:
+                    length = int(input("How long should the password be?\t"))
+                    if length != 0:
+                        break
+            case 3:
+                if SPECIAL_CHARACTERS not in CHARS:
+                    CHARS += SPECIAL_CHARACTERS
+                    use_special_chars = True
+            case 4:
+                if CAPITAL_LETTERS not in CHARS:
+                    CHARS += CAPITAL_LETTERS
+                    use_capital_letters = True
+            case 5:
+                if NUMBERS not in CHARS:
+                    CHARS += NUMBERS
+                    use_numbers = True
